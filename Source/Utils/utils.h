7bd12c57d2760288614d86a052bf5d7ab29bf2a4
Hugo Carricart <hugo.carricart@quantor.fr>
2025-01-15 22:22:41 +0100

    Changement archi projet.

---
+++ Source/Utils/utils.h
@@ -0,0 +1,11 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+namespace Utils
+{
+
+void print(const char* message);
+
+}
+
+#endif // UTILS_H
--- Source/main.cpp
+++ Source/main.cpp
@@ -1,7 +1,9 @@
+#include "Utils/utils.h"
 #include <iostream>
 
 int main(int argc, char *argv[])
 {
     std::cout << "Putain de char volant !" << std::endl;
+    Utils::print("Calme toi !");
     return 0;
 }
